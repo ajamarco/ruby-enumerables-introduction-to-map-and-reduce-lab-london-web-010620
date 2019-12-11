e6fdc89ad480375293dfabc8195d59ffe463fffb
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-11 23:04:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -36,6 +36,11 @@ def map_to_square(source_array)
 end 
 
 def reduce_to_total(source_array, starting_point = 0)
+  returned_value = starting_point
+  source_array.length.times do |index|
+    returned_value += source_array[index]
+  end 
+  returned_value
 end
 
 def reduce_to_all_true(source_array)
