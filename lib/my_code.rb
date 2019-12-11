1bc34b1707eb582c96a6aa594e48649424d06e19
Alexandre Jamarco <alexandre.j@gmail.com>
2019-12-11 22:55:02 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -39,6 +39,9 @@ def reduce_to_total(source_array, starting_point)
 end
 
 def reduce_to_all_true(source_array)
+  
+  source_array.length.times do |index|
+  end 
 end
 
 def reduce_to_any_true(source_array)
